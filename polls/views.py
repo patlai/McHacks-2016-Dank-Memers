196dc2a3e8a4761a7746ce541734caa59a960cce
Patrick Lai <patricklai10123@gmail.com>
2016-02-20 12:53:22 -0500

    dank memes

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("polls index")
+    return HttpResponse("jet fuel can't melt dank memes")
