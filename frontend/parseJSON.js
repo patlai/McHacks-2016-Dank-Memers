61d7e20e652ace8ddc80084d8c144881a75e0a82
Wai Lau <wailunlau@hotmail.com>
2016-02-20 22:03:02 -0800

    parseJSON

--- frontend/parseJSON.js
+++ frontend/parseJSON.js
@@ -1,6 +1,23 @@
-function getData(){
-    alert("hi");
-    var jqxhr = $.getJSON( "example.json", function() {
-    alert('hi');
-    })
+var fs = require('fs');
+
+fs.readFile('storage.json', function (err, data) {
+  if (err)
+    console.log(err);
+  var pdf = JSON.parse(data);
+  pdf.forEach(function (file) {
+    if (file.meeting.length > 5)
+      console.log('Too many classes!');
+    if (file.name === undefined)
+      console.log('no name!');
+    if (file.code === undefined)
+      console.log('no code');
+    if (file.teacher === undefined)
+      console.log(file);
+  });
+  console.log(pdf.length);
+});
+
+
+getData(){
+    alert(pdf.hello)
 }
